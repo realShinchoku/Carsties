27981565208dd371bf1eb21ff2c32947fc4b92b4
Shinchoku <tienmp3@gmail.com>
2023-09-17 11:53:51 +0700

    s6

---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,32 @@
+using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _context;
+
+    public BidPlacedConsumer(AuctionDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming BidPlaced");
+
+        var auction = await _context.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (auction == null) return;
+
+        if (auction.CurrentHighBind == null
+            || context.Message.BidStatus.Contains("Accepted")
+            && context.Message.Amount > auction.CurrentHighBind)
+        {
+            auction.CurrentHighBind = context.Message.Amount;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
